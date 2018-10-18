b6eaf612a216dc9883496a1eed6858b6f3f5e91e
elisa stater <elisa_60@msn.com>
2018-10-18 15:56:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ console.log(recipes, obj);
 
 function destructivelyUpdateObjectWithKeyAndValue() {
       
-      var newobj= {prop2: 2};
+      var newobj= {prop: 1, prop2: 2};
       var obj=Object.assign({},newobj);
 
 
